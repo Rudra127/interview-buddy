60a4493638b2e393c9099e9b5bca3c002be03029
Rudra127 <prajapatirudra127@gmail.com>
2025-05-29 21:00:36 +0530

    added landing page

---
+++ landing-page/src/pages/Index.tsx
@@ -0,0 +1,28 @@
+
+import Header from '@/components/Header';
+import HeroSection from '@/components/HeroSection';
+import FeaturesSection from '@/components/FeaturesSection';
+import HowItWorksSection from '@/components/HowItWorksSection';
+import TechHighlightsSection from '@/components/TechHighlightsSection';
+import TestimonialsSection from '@/components/TestimonialsSection';
+import PricingSection from '@/components/PricingSection';
+import FAQSection from '@/components/FAQSection';
+import Footer from '@/components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <HeroSection />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <TechHighlightsSection />
+      <TestimonialsSection />
+      <PricingSection />
+      <FAQSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
